e961b77baf18951311bbede9300f7b0ffca69acc
Gymmish <samaloanastasia@gmail.com>
2021-12-13 11:36:27 +0200

    ..

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = creatGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function creatGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link"  href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  modal.show();
+}
+
+const modal = basicLightbox.create(
+  `<div class="modal">
+   <img
+      class="gallery__image"
+      src = "data-source"
+    />
+    </div>`
+);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = creatGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function creatGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `<a class="gallery__item"
+     href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+
+const settingsForModal = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+const lightBox = new SimpleLightbox(".gallery a", settingsForModal);
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+}
